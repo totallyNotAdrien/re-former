ace15ff452a5047a598389de20e70fe604c8bb71
Tina Thompson <mark_junior13@yahoo.com>
2022-02-23 06:54:15 -0600

    make form Railsier with form_with

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,14 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-    #@user = User.new(user_params)
+    #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
 
     if @user.save
       redirect_to new_user_path
     else
       render :new
+      puts "milk"
     end
   end
 
